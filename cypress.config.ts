446bc899ff928bda5ffb01d42847f60722927da2
Thomas Plumpton <thomas.plumpton@matillion.com>
2025-08-29 11:02:24 +0100

    fix(repro): registered code coverage plugin in cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,17 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from "cypress"
+import codeCoverage from '@cypress/code-coverage/task'
 
 export default defineConfig({
   component: {
     devServer: {
       framework: "react",
-      bundler: "vite",
+      bundler: "vite"
     },
-  },
-});
+    setupNodeEvents(
+      on: Cypress.PluginEvents,
+      config: Cypress.PluginConfigOptions
+    ) {
+      return codeCoverage(on, config)
+    }
+  }
+})
