f7a673a0fdc76f6a02feb79e02433f19f6391526
Thomas Plumpton <thomas.plumpton@matillion.com>
2025-08-29 10:46:46 +0100

    feat(repro): fix lint and build

--- cypress/support/component.ts
+++ cypress/support/component.ts
@@ -3,6 +3,7 @@ import { mount } from 'cypress/react'
 import '@cypress/code-coverage/support'
 
 declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
   namespace Cypress {
     interface Chainable {
       mount: typeof mount
