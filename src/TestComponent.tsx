5b9dd126b64afdae6dc5c87ec1f2313e28b983ff
Thomas Plumpton <thomas.plumpton@matillion.com>
2025-08-29 12:36:00 +0100

    chore(repro): added comments to test component

--- src/TestComponent.tsx
+++ src/TestComponent.tsx
@@ -6,16 +6,25 @@ export interface TestComponentProps {
 
 export const TestComponent = ({ someProperty, onSomeEvent, onAnotherEvent }: TestComponentProps) => {
   const handleClick = () => {
+    // The first 2 branches.
+    // The unit test checks less than 5 for the truthy case.
+    // The cypress test will hit the implicit else here via the falsy case.
     if (someProperty < 5) {
       onSomeEvent(someProperty)
     }
 
+
+    // The second 2 branches.
+    // The cypress test checks greater than 10 for the truthy case.
+    // The unit test will hit the implicit else hre via the falsy case.
     if (someProperty > 10) {
       onAnotherEvent(someProperty)
     }
   }
 
   // A redundant ternary to force Istanbul to consider it as a branch to be instrumented.
+  // The unit and cypress tests hit the falsy case for a non-zero value.
+  // The cypress test alone hits the truthy case for a zero value.
   const isDisabled = someProperty === 0 ? true : false
 
   return (
