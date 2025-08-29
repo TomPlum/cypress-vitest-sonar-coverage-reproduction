af97ae786930345d622a4cdb5e96a7124109baa9
evdokiamina <evdokia.mina@matillion.com>
2025-08-29 16:30:47 +0100

    trial: without swc

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig, defaultExclude } from 'vitest/config'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 import istanbul from 'vite-plugin-istanbul'
 import dts from 'vite-plugin-dts'
 import {resolve} from "path";
